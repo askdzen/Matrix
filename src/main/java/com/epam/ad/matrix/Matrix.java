f4c20826b8b4833afd1dbeac6aea5b0c63603b69
askdzen <ask.dzen@mail.ru>
2014-06-06 12:08:10 +0600

    Initial commit

---
+++ src/main/java/com/epam/ad/matrix/Matrix.java
@@ -0,0 +1,35 @@
+package com.epam.ad.matrix;
+
+
+final public class Matrix {
+    private final int M;             // number of rows
+    private final int N;             // number of columns
+    public double[][] data;
+
+    public Matrix(int M, int N) {
+        this.M = M;
+        this.N = N;
+        data = new double[M][N];
+    }
+
+
+    public static Matrix random(int M, int N) {
+        Matrix A = new Matrix(M, N);
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < N; j++)
+                A.data[i][j] = Math.random();
+        }
+        return A;
+    }
+
+
+
+    public void show() {
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++)
+                System.out.printf("%9.4f ", data[i][j]);
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/com/epam/ad/matrix/Program.java
@@ -0,0 +1,27 @@
+package com.epam.ad.matrix;
+
+public class Program {
+    public static void main(String[] args) {
+        int col =5;
+        int row =5;
+
+        Matrix A = Matrix.random(col, row);
+        Matrix B = Matrix.random(col, row);
+        A.show();
+        System.out.println();
+
+        B.show();
+        System.out.println();
+
+        Matrix C = new Matrix(col,row);
+        for (int i = 0; i < col; i++)
+        {
+            for (int j = 0; j < row; j++)
+                C.data[i][j] += (A.data[i][j] * B.data[i][j]);
+        }
+        C.show();
+
+
+
+    }
+}
