68977d381f4a7c196fa32263b0c828a7e7e6e01f
askdzen <ask.dzen@mail.ru>
2014-06-08 18:45:36 +0600

    multiply method create refactoring

--- src/main/java/com/epam/ad/matrix/Program.java
+++ src/main/java/com/epam/ad/matrix/Program.java
@@ -1,10 +1,18 @@
 package com.epam.ad.matrix;
 
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        int col =5;
-        int row =5;
+        System.out.println("Введите количество столбцов в матрице...");
+        Scanner c = new Scanner(System.in);
+        int col=c.nextInt();
+        System.out.println();
+        System.out.println("Введите количество строк в матрице...");
+        Scanner r = new Scanner(System.in);
+        System.out.println();
 
+        int row=r.nextInt();
         Matrix A = Matrix.random(col, row);
         Matrix B = Matrix.random(col, row);
         A.show();
